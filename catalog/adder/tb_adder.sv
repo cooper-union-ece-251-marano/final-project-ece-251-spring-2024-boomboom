69905b08d7b56e329f7b97eccba52d4614050418
Jonghyeok Kim <59267186+BurtKim-ust@users.noreply.github.com>
2024-04-16 21:51:49 -0400

    Update tb_adder.sv

--- catalog/adder/tb_adder.sv
+++ catalog/adder/tb_adder.sv
@@ -20,19 +20,21 @@ module tb_adder;
     parameter n = 32;
     logic [(n-1):0] a, b, y;
 
-   initial begin
+    initial begin
         $dumpfile("adder.vcd");
-        $dumpvars(0, uut);
+        $dumpvars(0, tb_adder);
         $monitor("a = 0x%0h b = 0x%0h y = 0x%0h", a, b, y);
     end
 
     initial begin
-        a <= #n'hFFFFFFFF;
-        b <= #n'hFFFFFFFF;
+        a <= #10 {n{1'b1}};
+        b <= #10 {n{1'b1}};
+        #100; // Delay for simulation
+        $finish; // Finish simulation
     end
 
     adder uut(
-        .A(a), .B(b), .Y(y)
+        .A(a), .A(b), .SUM(y)
     );
 endmodule
 `endif // TB_ADDER
