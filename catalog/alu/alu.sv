6077d4f31227c3412b8f17a4d558c28d39f5b0ba
Jonghyeok Kim <59267186+BurtKim-ust@users.noreply.github.com>
2024-04-29 00:06:50 -0400

    Update alu.sv

--- catalog/alu/alu.sv
+++ catalog/alu/alu.sv
@@ -8,7 +8,6 @@
 //     Description: 32-bit RISC-based CPU alu (MIPS)
 //
 // Revision: 1.0
-// see https://github.com/Caskman/MIPS-Processor-in-Verilog/blob/master/ALU32Bit.v
 //////////////////////////////////////////////////////////////////////////////////
 `ifndef ALU
 `define ALU
@@ -16,46 +15,50 @@
 `timescale 1ns/100ps
 
 module alu
-    #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    input logic [n-1:0] A,B,
-    input logic [3:0] FUNCT,
-    output logic [n-1:0] Y, Hi, Lo,
-    output logic zero
-
-    //
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-
+    #(parameter n = 32)
+    (
+        input logic [n-1:0] A, B,
+        input logic [3:0] FUNCT,
+        output logic [n-1:0] Y, Hi, Lo,
+        output logic zero
+    );
     logic [n+n-1:0] Hilo;
 
+    // Zero flag logic correction
+    assign zero = (Y == {n{1'b0}});
 
-    assign zero = (result == {n{1'b0}});
-    always @(A or B or FUNCT) begin
-	    case(FUNCT)
-		    4'b0001: Y = A + B; //add
-		    4'b0010: Y = A - B; //sub
-		    4'b0011: Hilo = A*B; //mult
-			     Hi = Hilo[n+n-1:n];
-			     Lo = Hilo[n-1:0];
-		    4'b0100: Lo = A/B; //div
-		    	     Hi = A%B;
-		    4'b0100: Y = A/B;
-		    4'b0101: Y = A|B; //or
-		    4'b0110: Y = A&B; //and
-		    4'b0111: Y = ~(A|B); //nor
-		    4'b1000: Y = A^B; //xor
-		    4'b1001: Y = A<<B; //sll
-		    4'b1010: Y = A>>B; //srl
-		    4'b1011: Y = (A < B) ? 1:0; //slt
-		endcase
-	end
+    // ALU operations definition
+    always @* begin
+        
+        case(FUNCT)
+            4'b0001: Y = A + B; // Add
+            4'b0010: Y = A - B; // Subtract
+            4'b0011: begin // Multiply
+                Hilo = A * B;
+                Hi = Hilo[n+n-1:n];
+                Lo = Hilo[n-1:0];
+                Y = Lo; // To be determined later
+            end
+            4'b0100: begin // Divide
+                Y = A / B; 
+                Hi = A % B;
+            end
+            4'b0101: Y = A | B; // OR
+            4'b0110: Y = A & B; // AND
+            4'b0111: Y = ~(A | B); // NOR
+            4'b1000: Y = A ^ B; // XOR
+            4'b1001: Y = A << B; // Shift left logical
+            4'b1010: Y = A >> B; // Shift right logical
+            4'b1011: Y = (A < B) ? 1 : 0; // Set less than
 
-		    
-
+            4'b1111: begin  // Reset
+                Y = 1'bx; 
+                Hi = 1'bx; 
+                Lo = 1'bx; 
+                Hilo = 1'bx;
+            end
+        endcase
+    end
 
 endmodule
 
