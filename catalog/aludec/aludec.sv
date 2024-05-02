b6af1105cefb889d9b931f0475577390cb5d0b99
Jonghyeok Kim <59267186+BurtKim-ust@users.noreply.github.com>
2024-05-02 00:55:02 -0400

    Update aludec.sv

--- catalog/aludec/aludec.sv
+++ catalog/aludec/aludec.sv
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Anthony Kwon, Jonghyeok(Burt) Kim
 // 
-//     Create Date: 2023-02-07
+//     Create Date: 2024-05-02
 //     Module Name: aludec
 //     Description: 32-bit RISC ALU decoder
 //
@@ -17,32 +17,28 @@
 
 module aludec
     #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
+        input logic [3:0] funct,
+        input logic [1:0] aluop,
+        output logic [3:0] aluctrl
+    );
 
-    input logic [3:0] funct;
-    input logic [1:0] aluop;
-    output logic [3:0] aluctrl;
+    // Default initialization
+    initial aluctrl = 4'b0000;
 
-
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    
+    // Combinational logic to determine ALU control signal
     always @* begin
-	    case(aluop)
-	    	2'b01: aluctrl <= 4'b0010; // sub for BEQ
+        case(aluop)
+            2'b01: aluctrl <= 4'b0010; // sub for BEQ
 	    	2'b10: aluctrl <= 4'b0001; // add for LW,SW,ADDI
 	    	2'b11: aluctrl <= 4'bxxxx; // for when not in use
-		default: begin
-			case(funct)
-				aluctrl <= funct;
-				default:
-					aluctrl <= 4'bxxxx;
-			endcase
-	    	end
-	    endcase
+            default: begin
+                if (funct > 0) begin
+                aluctrl <= funct;
+            end else begin
+                aluctrl <= 4'b0000;
+            end
+            end
+        endcase
     end
 
 endmodule
