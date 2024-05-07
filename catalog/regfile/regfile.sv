4e7f6ff98a1add1b74d3e1f23f3071ac0ded54d4
atkwon <atkwon20@gmail.com>
2024-05-07 00:22:21 -0400

    hard code zero reg

--- catalog/regfile/regfile.sv
+++ catalog/regfile/regfile.sv
@@ -30,7 +30,7 @@ module regfile
     //
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     //
-    logic [(n-1):0] rf[(2**7-1):0];
+    logic [(n-1):0] rf[(2**r-1):0];
 
     // three ported register file
     // read two ports combinationally
@@ -39,11 +39,16 @@ module regfile
     // note: for pipelined processor, write third port
     // on falling edge of clk
 
-    always @(posedge clk)
-        if (we3) rf[wa3] <= wd3;	
+    always begin
+            rf[0] <= 0;
+            @(posedge clk)
+                if (we3) rf[wa3] <= wd3;
 
-    assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
-    assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
+                    assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
+                    assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
+    end
 endmodule
+
+`endif // REGFILE
 
 `endif // REGFILE
