52668ec48e70fc7a2290967a95914d4e5b329984
atkwon <atkwon20@gmail.com>
2024-05-07 00:13:52 -0400

    Updated register size

--- catalog/regfile/regfile.sv
+++ catalog/regfile/regfile.sv
@@ -30,7 +30,7 @@ module regfile
     //
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     //
-    logic [(n-1):0] rf[(2**5-1):0];
+    logic [(n-1):0] rf[(2**7-1):0];
 
     // three ported register file
     // read two ports combinationally
